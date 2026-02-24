7f23fefb7c27cfa6ddde52fefd8526adfb30c9ca
한이 <choihanni27@gmail.com>
2026-02-24 16:45:29 +0900

    코더스 로고 이미지 배치, 헤더 완성

--- src/components/main/Page1.jsx
+++ src/components/main/Page1.jsx
@@ -1,10 +1,35 @@
-function Page1 (){
-    return (
-        <div>
-            <h1>Page 1</h1>
-            <p>This is the content of Page 1.</p>
-        </div>
-    );
+
+// src/components/main/Page1.jsx
+import "./css/Page1.css";
+
+function Page1() {
+  return (
+    <div className="page1">
+      {/* 헤더 */}
+      <header className="header">
+        <img src="/logo.png" alt="Code-Us" className="logo" style={{ width: "120px" }} />
+
+        <nav className="nav">
+          <span>Introduce</span>
+          <span>Project</span>
+          <span>Support</span>
+          <span className="blog">
+            Blog <span className="arrow">↗</span>
+          </span>
+        </nav>
+      </header>
+
+      {/* 메인 히어로 영역 */}
+      <main className="hero">
+        <h1>
+          <span className="highlight">4기</span> 멤버를 찾습니다
+        </h1>
+        <p>실무의 길로, 함께 나아가는 IT 동아리</p>
+
+        <button className="apply-btn">지원하기</button>
+      </main>
+    </div>
+  );
 }
 
 export default Page1;
