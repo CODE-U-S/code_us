733dfac64c32e1f2572d0c84452c61c77d8e7929
gushgus <s2547@e-mirim.hs.kr>
2026-02-24 16:51:17 +0900

    브랜치 생성

---
+++ src/components/main/Page5.jsx
@@ -0,0 +1,14 @@
+// page5.jsx
+
+import React from "react";
+
+function Page5() {
+  return (
+    <div>
+      <h1>Page 5 입니다</h1>
+      <p>React 연습용 페이지입니다.</p>
+    </div>
+  );
+}
+
+export default Page5;
