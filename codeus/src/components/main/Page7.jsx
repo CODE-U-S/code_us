b7c1b2e4c610ff3c04f5ff83702d7b4c2252c505
조현서 <hyeonseojo929@gmail.com>
2026-03-10 12:42:03 +0900

    chore: apply latest Page7 updates

--- codeus/src/components/main/Page7.jsx
+++ codeus/src/components/main/Page7.jsx
@@ -16,13 +16,13 @@ function Page7() {
           <p>미림마이스터고등학교 전공자율동아리</p>
           <p>Email: codeus24@gmail.com</p>
           <p className="copyright">
-            Copyright © 2024 CODEUS Inc. All Rights Reserved
+            Copyright © 2026 CODEUS Inc. All Rights Reserved
           </p>
         </div>
 
         {/* 오른쪽 정보 */}
         <div className="footer-right">
-          <p className="version">2024 Ver. Created By 1기</p>
+          <p className="version">2026 Ver. Created By 3~4기</p>
           <div className="line"></div>
 
           <div className="icons">
